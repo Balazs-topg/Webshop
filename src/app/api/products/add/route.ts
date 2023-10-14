08f20594f0691ebb486515ab61d31cf5ff92b1d2
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2023-10-14 15:25:35 +0200

    Add ability to store imgs on db

--- src/app/api/products/add/route.ts
+++ src/app/api/products/add/route.ts
@@ -1,51 +1,63 @@
 import { NextResponse, NextRequest } from "next/server";
 import jwt from "jsonwebtoken";
-
 import accountModel from "../../models/accountModel";
 import productModel from "../../models/productModel";
-
 import "../../utils/connectToDB";
 
-type AddItemType = {
-  brand: string;
-  category: string;
-  imgs: File[];
-  name: string;
-  tags: string[];
-  price: number;
-};
-
-export async function POST(request: NextRequest, response: any) {
+export async function POST(request: NextRequest) {
   console.log("request recived!");
 
-  const reqBody: AddItemType = await request.json();
+  const formData = await request.formData();
+  const reqBody: any = {};
+
+  for (let [key, value] of formData.entries()) {
+    if (key.startsWith("img")) {
+      const file = value as any;
+      const chunks: any[] = [];
+      for await (const chunk of file.stream()) {
+        chunks.push(chunk);
+      }
+      const buffer = Buffer.concat(chunks);
+      reqBody[key] = buffer.toString("base64");
+    } else {
+      reqBody[key] = value;
+    }
+  }
+
+  // Convert img0, img1, img2, etc. to an img array
+  const imgArray = [];
+  for (let i = 0; reqBody.hasOwnProperty(`img${i}`); i++) {
+    imgArray.push(reqBody[`img${i}`]);
+    delete reqBody[`img${i}`]; // Remove the img0, img1, etc. properties from the object
+  }
+  // Add the img array to the object
+  reqBody.imgs = imgArray;
+
+  console.log(reqBody);
 
   const reqJwt = request.headers.get("jwt");
 
-  //authenticate jwt - if invalid then return 40
+  // Authenticate JWT
   if (!reqJwt) return NextResponse.json({}, { status: 401 });
   const decodedJwt = jwt.decode(reqJwt);
   if (!(decodedJwt && typeof decodedJwt === "object" && "id" in decodedJwt))
     return NextResponse.json({}, { status: 401 });
   const userId = decodedJwt.id;
-  if (!userId) return false;
+  if (!userId) return NextResponse.json({}, { status: 401 });
   const user = await accountModel.findById(userId);
   const isAdmin = user ? user.isAdmin : false;
   if (!isAdmin) return NextResponse.json({}, { status: 401 });
 
-  //check if name alredy exists
+  // Check if name already exists
   const name = (await productModel.countDocuments({ name: reqBody.name })) > 0;
   if (name) return NextResponse.json({}, { status: 400 });
 
-  //push to db
-
-  //chat
-
+  // Push to db
   const newProduct = new productModel({
     name: reqBody.name,
     brand: reqBody.brand,
     category: reqBody.category,
-    imgs: "",
+    imgs: reqBody.imgs,
     tags: reqBody.tags,
     price: reqBody.price,
   });
@@ -57,11 +69,4 @@ export async function POST(request: NextRequest, response: any) {
   );
 }
 
-//boiler plate
-export async function GET(request: Request) {}
-export async function HEAD(request: Request) {}
-// export async function POST(request) {}
-export async function PUT(request: Request) {}
-export async function DELETE(request: Request) {}
-export async function PATCH(request: Request) {}
-export async function OPTIONS(request: Request) {}
+// ... other routes
